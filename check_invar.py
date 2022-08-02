488465ab2f67e97b6b9c72fa0cc93d9442c53675
ycwan9 <ycwan9@outlook.com>
2022-08-02 20:49:02 +0800

    enable some clang's extra check

--- check_invar.py
+++ check_invar.py
@@ -55,7 +55,22 @@ def diff_exe(u, o):
     return diff_trace(record(u), record(o))
 
 
-CFALGS = ["-g"]
+CFALGS = [
+    "-Werror=conditional-uninitialized",
+    "-Werror=format-insufficient-args",
+    "-Werror=format-pedantic",
+    "-Werror=implicit-function-declaration",
+    "-Werror=implicit-int",
+    "-Werror=incompatible-library-redeclaration",
+    "-Werror=incompatible-pointer-types",
+    "-Werror=int-conversion",
+    "-Werror=pedantic",
+    "-Werror=return-type",
+    "-Werror=sometimes-uninitialized",
+    "-Werror=uninitialized",
+    "-Werror=uninitialized-const-reference",
+    "-g"
+]
 CFALGS_UNOPT = ["-O0"]
 CFALGS_OPT = ["-O2"]
 CLANG = "clang"
